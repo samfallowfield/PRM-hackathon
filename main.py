50e4c70b8c67ff5e4a85f423fbeed8de0d0d7a7d
CatherineDeas <98884054+CatherineDeas@users.noreply.github.com>
2024-03-26 12:29:41 +0000

    add confidence rating

--- main.py
+++ main.py
@@ -10,16 +10,15 @@ def extract_text_from_image(image_path):
 
     # call textract
     response = textract_client.detect_document_text(Document={'Bytes': image_bytes})
-    print(response)
 
     extracted_text = ''
     for item in response['Blocks']:
         if item['BlockType'] == 'LINE':
             extracted_text += item['Text'] + '\n'
-
+            print(item['Confidence'])
     return extracted_text
 
 
-image_path = 'images/gp_note2.png'
+image_path = 'images/gp_note4.png'
 final_extracted_text = extract_text_from_image(image_path)
 print(final_extracted_text)
