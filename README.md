78649ccdb459237cc3efbd8ae0e2ac7f7e18b098
sam fallowfield <sam.fallowfield1@gmail.com>
2024-03-26 10:50:49 +0000

    written initial textract function

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-test
+To install all python packages run:
+- pip install -r requirements.txt
+
+
+Current Goals for the Hackathon
+- Find an OCR tool to extract text for existing patient records/ doctors notes.
+- Storing initial images inside an S3 bucket
+- Configure script to take images from S3 and run through AWS Text extract
+- Undecided on final format / storage solution for final data.
---
+++ main.py
@@ -0,0 +1,24 @@
+import boto3
+
+
+def extract_text_from_image(image_path):
+    textract_client = boto3.client('textract')
+
+    # read image file
+    with open(image_path, 'rb') as image_file:
+        image_bytes = image_file.read()
+
+    # call textract
+    response = textract_client.detect_document_text(Document={'Bytes': image_bytes})
+
+    extracted_text = ''
+    for item in response['Blocks']:
+        if item['BlockType'] == 'LINE':
+            extracted_text += item['Text'] + '\n'
+
+    return extracted_text
+
+
+image_path = 'images/gp_note1.png'
+final_extracted_text = extract_text_from_image(image_path)
+print(final_extracted_text)
