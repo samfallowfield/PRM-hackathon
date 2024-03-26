6357e5faf87f69dc14649ada64b4aeb5e9748653
aburathoki1 <asha.burathoki@madetech.com>
2024-03-26 15:14:34 +0000

    WIP: implement S3

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import boto3
 
 
-def extract_text_from_image(image_path):
+# def extract_text_from_image(image_path=none):
+def extract_text_from_image():
     textract_client = boto3.client('textract')
 
     # read image file
-    with open(image_path, 'rb') as image_file:
-        image_bytes = image_file.read()
+    # with open(image_path, 'rb') as image_file:
+    #     image_bytes = image_file.read()
 
     # call textract
-    response = textract_client.detect_document_text(Document={'Bytes': image_bytes})
+    # response = textract_client.detect_document_text(Document={'Bytes': image_bytes})
+
+    s3_bucket = 'prm-hackathon-record'
+
+    document_key = 'example7.png'
+    response = textract_client.detect_document_text(
+        Document={
+            'S3Object': {
+                'Bucket': s3_bucket,
+                'Name': document_key
+            }
+        })
 
     extracted_text = ''
+
     for item in response['Blocks']:
         if item['BlockType'] == 'LINE':
             if item['Confidence'] > 90:
@@ -22,6 +35,12 @@ def extract_text_from_image(image_path):
     return extracted_text
 
 
-image_path = 'images/gp_note5.png'
-final_extracted_text = extract_text_from_image(image_path)
+# image_path = 'images/gp_note5.png'
+# final_extracted_text = extract_text_from_image(image_path)
+final_extracted_text = extract_text_from_image()
+with open("test.txt", "w") as f:
+    f.write(final_extracted_text)
+
+
+uplo
 print(final_extracted_text)
